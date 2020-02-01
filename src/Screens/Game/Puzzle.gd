acc025a6f275d562c751df33661b1da72f289d4c
AJ Dunn <andrew.dunn2@outlook.com.au>
2020-02-01 15:48:46 +1030

    andrews updates

--- src/Screens/Game/Puzzle.gd
+++ src/Screens/Game/Puzzle.gd
@@ -4,7 +4,7 @@ class_name Puzzle
 # Declare member variables here. Examples:
 var scene_piece = null
 
-var level = 4
+var level = 1
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -12,15 +12,18 @@ func _ready():
 	for i in range(level):
 		var ran = make_rando_pieces()
 		add_rando_pieces(i, ran)
-		
+
+
 func start_new_level(num):
 	level  = num
-	var getType = self.get_meta("type")    # get the type of myNode
-	
+	var getType = self.get_meta("type") # get the type of myNode
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if Input.is_action_pressed("ui_up"):
+		print("right")
+
 
 func make_rando_pieces():
 	var ran = randi() % GameData.PIECES.size()
@@ -36,16 +39,32 @@ func make_rando_pieces():
 #		GameData.PIECES.TEAR:
 #			print("my tear")
 	return ran
-	
+
+
 func add_rando_pieces(count, ran):
 	var scene_piece_instance = scene_piece.instance()
+	
+	var y_line = 0 if (count < 5) else 300
+	var x_line = 350 if (count < 5) else 350 - (5 * 300)
+	
 	scene_piece.set_meta("type", "gamer")
 	scene_piece_instance.set_name("scene")
-	scene_piece_instance.global_position.x = count * 300 + 500
-	scene_piece_instance.global_position.y = 300
+	scene_piece_instance.global_position.x = count * 300 + x_line
+	scene_piece_instance.global_position.y = 300 + y_line
 	scene_piece_instance.change_piece(ran)
 	scene_piece_instance.hide()
 	scene_piece_instance.time_til_fade = 1.1 * count
 	$Game.add_child(scene_piece_instance)
-	
-	
+
+
+func _input(event):
+	if event is InputEventMouseButton:
+		if event.button_index == BUTTON_LEFT and event.pressed:
+			print("Left button was clicked at ", event.position)
+		if event.button_index == BUTTON_WHEEL_UP and event.pressed:
+			print("Wheel up")
+
+
+func _on_Touch_selected_moon():
+	print("user selected moon okay?")
+	pass # Replace with function body.
--- src/Screens/Game/Touch.gd
+++ src/Screens/Game/Touch.gd
@@ -1,21 +1,24 @@
-extends Node
+extends Node2D
 
+signal selected_moon
 
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
 
-enum PIECES {HEART, MOON, SQUARE, STAR, TEAR}
-var hand_cursor = load("res://src/assets/final/CURSOR_HAND_1.png")
-
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	randomize()
-	Input.set_custom_mouse_cursor(hand_cursor)
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	pass
+
+
+func _on_MoonArea2D_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton:
+		if !event.pressed:
+			emit_signal("selected_moon")
+	pass # Replace with function body.
