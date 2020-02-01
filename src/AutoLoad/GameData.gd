34d4eac91021f307734c4d293c76616b80770eb0
velanoble <47091951+velanoble@users.noreply.github.com>
2020-02-01 18:21:32 +1030

    Puzzle Shapes refines

--- src/AutoLoad/GameData.gd
+++ src/AutoLoad/GameData.gd
@@ -8,7 +8,6 @@ extends Node
 enum PIECES {HEART, MOON, SQUARE, STAR, TEAR}
 var hand_cursor = load("res://src/assets/final/CURSOR_HAND_1.png")
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	randomize()
