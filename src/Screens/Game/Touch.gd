58250a1eabe942bbf9e0d067c71dc1c6419bc7f9
AJ Dunn <andrew.dunn2@outlook.com.au>
2020-02-01 20:05:25 +1030

    touch down the bottom

--- src/Screens/Game/Touch.gd
+++ src/Screens/Game/Touch.gd
@@ -9,3 +9,19 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_MoonArea2D_mouse_entered():
+	print("_on_MoonArea2D_mouse_entered");
+	pass # Replace with function body.
+
+
+func _on_MoonArea2D_mouse_exited():
+	print("_on_MoonArea2D_mouse_exited");
+	pass # Replace with function body.
+
+
+func _on_MoonArea2D_input_event(viewport, event, shape_idx):
+	if (event is InputEventMouseButton and event.is_pressed()):
+		print("clicky")
+	pass # Replace with function body.
